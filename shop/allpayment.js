b0f68de6db330f2ccbbc3bf981a81c2b9fd3e4af
rasssya76 <80687852+rasssya76@users.noreply.github.com>
2021-11-22 11:24:21 +0700

    Add files via upload

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,31 +1,31 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT
 
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ GA ADA
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ GA PUNYA
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ GA PUNYA
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ GA PUNYA
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ GA PUNYA
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ GA PUNYA
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ GA ADA
 	
 Semuanya atas nama RI****DI
 
 *📲 PULSA*
 
-Tidak tersedia
+081515589573
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
